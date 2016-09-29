a88d88ea2f61a094184f2b212dd598708ebdce20
orange-beans <cczhang@simtech.a-star.edu.sg>
2016-09-29 23:49:22 +0800

    Add Flasher class to lib; Tested Ticker, RTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
 #include "mbed.h"
+#include "Flasher.h"
 
-DigitalOut myled1(LED1);
-DigitalOut myled2(LED2);
+Ticker flipper;
+Ticker flipper2;
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+//DigitalOut led3(LED3);
+//DigitalOut led4(LED4);
 
-PwmOut myled3(LED3);
 
-const float DELAY_TIME = 0.012;
+void flip2() {
+  led1 = !led1;
+}
+
+void flip() {
+  led2 = !led2;
+}
+
+Flasher led3(LED3);
+Flasher led4(LED4, 2);
 
 int main() {
-
-  myled3.period(0.02);
-  myled3.write(0.1);
-
+  led2 = 1;
+  flipper.attach(&flip, 1); // the address of the function to be attached (flip) and the interval (2 seconds)
+  flipper2.attach(&flip2, 1);
+  // spin in a main loop. flipper will interrupt it to call flip
   while(1) {
-      myled1 = 1;
-      myled2 = 0;
-      wait(DELAY_TIME);
-      myled1 = 0;
-      myled2 = 1;
-      wait(DELAY_TIME);
+    led3.flash(3);
+    led4.flash();
   }
 }
