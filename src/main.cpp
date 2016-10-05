52a0d1c886e2de39f1a3eeaa64843a264784eaf3
orange-beans <cczhang@simtech.a-star.edu.sg>
2016-10-05 17:41:05 +0800

    Add cJSON support; setup basic UART COM

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "mbed.h"
+#include <string>
+#include <cJSON.h>
 #include <Flasher.h>
+Serial pc(USBTX, USBRX);
 
 Ticker flipper;
 Ticker flipper2;
@@ -16,16 +19,48 @@ void flip() {
   led2 = !led2;
 }
 
+void readPC() {
+  // Note: you need to actually read from the serial to clear the RX interrupt
+  //char _buffer[128];
+  string test_str = "{\"name\":5}";
+  string holder;
+  //string temp;
+  cJSON *json;
+  int count;
+  json = cJSON_Parse(test_str.c_str());
+  if (!json) {
+    printf("Error before: [%s]\n", cJSON_GetErrorPtr());
+    //wait(2.0);
+  } else {
+    count = cJSON_GetObjectItem(json, "name")->valueint;
+    cJSON_Delete(json);
+  }
+  char temp;
+  while(temp != '\n') {
+    temp = pc.getc();
+    holder += temp;
+    //pc.gets(_buffer, 4);
+    //temp += _buffer;
+  }
+  //printf("%s\n", temp.c_str());
+  printf("%s\n", holder.c_str());
+  printf("%d\n", count);
+  led1 = !led1;
+}
+
 Flasher led3(LED3);
 Flasher led4(LED4, 2);
 
 int main() {
   led2 = 1;
+  pc.attach(&readPC);
   flipper.attach(&flip, 1); // the address of the function to be attached (flip) and the interval (2 seconds)
-  flipper2.attach(&flip2, 1);
+  //flipper2.attach(&flip2, 1);
   // spin in a main loop. flipper will interrupt it to call flip
   while(1) {
-    led3.flash(3);
+    led3.flash(1);
     led4.flash();
+    pc.printf("testing\n");
+    //wait(1.0f);
   }
 }
